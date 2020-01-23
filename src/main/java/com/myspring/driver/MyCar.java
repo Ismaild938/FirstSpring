4faa560c240440efb5cc4fb771b87d57d625ffc6
Ismaild938 <59448842+Ismaild938@users.noreply.github.com>
2020-01-23 23:10:43 +0530

    modified the property file

--- src/main/java/com/myspring/driver/MyCar.java
+++ src/main/java/com/myspring/driver/MyCar.java
@@ -5,6 +5,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MyCar {
+    
     public static void main(String[] args) {
 
         ApplicationContext c = new ClassPathXmlApplicationContext("application-context.xml");
